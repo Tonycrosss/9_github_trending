527374ba6faf18980a4ff52f3a85206234dc6187
mpak <starovoitov.anton@gmail.com>
2017-10-29 17:22:37 +0500

    Находит топ 20 репо по критериям

--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,25 @@
+import requests
+import datetime
+
+for_a_week = (datetime.datetime.today() - datetime.timedelta(days=7)).strftime('%Y-%m-%d')
 
 
-def get_trending_repositories(top_size):
-    pass
+def get_trending_repositories():
+    response = requests.get('https://api.github.com/search/repositories?'
+                            'q=created:>={}&sort=stars'
+                            '&order=desc'.format(for_a_week))
+    all_repos = response.json()
+    return all_repos
+
+
+def get_top_20(repos):
+    top_20_repos = repos['items'][0:20]
+    return top_20_repos
 
 
 def get_open_issues_amount(repo_owner, repo_name):
     pass
 
+
 if __name__ == '__main__':
-    pass
+    get_trending_repositories()
