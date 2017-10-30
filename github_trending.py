973c56abdf2b3c5ae039c6777b3013b50c4cc923
mpak <starovoitov.anton@gmail.com>
2017-10-31 00:22:22 +0500

    Рефакторинг функции

--- github_trending.py
+++ github_trending.py
@@ -16,25 +16,33 @@ def get_trending_repositories(quantity):
 
 def print_data_from_repos(repos):
     for repo in repos:
-        repo_name = repo['name']
-        repo_owner = repo['owner']['login']
-        repo_issues_quantity = get_repo_issue_quantity(repo_owner, repo_name)
-        print('Название репозитория: {}'.format(repo_name))
-        print('Имя владельца репозитория: {}'.format(repo_owner))
-        print('Количество открытых issues: {}'.format(repo_issues_quantity))
-        print('Ссылка на репозиторий: {}'.format(repo['html_url']))
+        print('Название репозитория: {}'.format(repo['repo_name']))
+        print('Имя владельца репозитория: {}'.format(repo['repo_owner']))
+        print('Количество открытых issues: {}'.format(
+            repo['repo_issues_quantity']))
+        print('Ссылка на репозиторий: {}'.format(repo['repo_html_url']))
         print('-' * 80)
 
 
-def get_repo_issue_quantity(repo_owner, repo_name):
-    response = requests.get('https://api.github.com/repos/{}/{}/issues'.format(
-                                                        repo_owner, repo_name))
-    issues_data = response.json()
-    issues_quantity = len(issues_data)
-    return issues_quantity
+def get_repo_clean_info(repos):
+    clean_repo_info = []
+    for repo in repos:
+        repo_owner = repo['owner']['login']
+        repo_name = repo['name']
+        repo_html_url = repo['html_url']
+        response = requests.get('https://api.github.com/repos/{}/{}/issues'.
+                                format(repo_owner, repo_name))
+        issues_data = response.json()
+        issues_quantity = len(issues_data)
+        clean_repo_info.append({'repo_name': repo_name,
+                                'repo_owner': repo_owner,
+                                'repo_issues_quantity': issues_quantity,
+                                'repo_html_url': repo_html_url})
+    return clean_repo_info
 
 
 if __name__ == '__main__':
     repos_quantity = 20
     trending_repos = get_trending_repositories(repos_quantity)
-    print_data_from_repos(trending_repos)
+    clean_repos_info = get_repo_clean_info(trending_repos)
+    print_data_from_repos(clean_repos_info)
