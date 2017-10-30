1fe0d1588ff362040a1b3afa58594b372a207ba2
mpak <starovoitov.anton@gmail.com>
2017-10-30 22:25:52 +0500

    убрал в поиск открытых проблем в отдельную функцию

--- github_trending.py
+++ github_trending.py
@@ -1,6 +1,7 @@
 import requests
 import datetime
 
+
 def get_trending_repositories():
     weekly = (datetime.datetime.today()
               - datetime.timedelta(days=7)).strftime('%Y-%m-%d')
@@ -19,11 +20,22 @@ def get_top(repos, quantity):
 
 def print_data_from_repos(repos):
     for repo in repos:
-        print('Название репозитория: {}'.format(repo['name']))
-        print('Количество открытых issues: {}'.format(
-                                                    repo['open_issues_count']))
+        repo_name = repo['name']
+        repo_owner = repo['owner']['login']
+        repo_issues_quantity = get_repo_issue_quantity(repo_owner, repo_name)
+        print('Название репозитория: {}'.format(repo_name))
+        print('Имя владельца репозитория: {}'.format(repo_owner))
+        print('Количество открытых issues: {}'.format(repo_issues_quantity))
         print('Ссылка на репозиторий: {}'.format(repo['html_url']))
         print('-' * 80)
+
+
+def get_repo_issue_quantity(repo_owner, repo_name):
+    response = requests.get('https://api.github.com/repos/{}/{}/issues'.format(
+                                                        repo_owner, repo_name))
+    issues_data = response.json()
+    issues_quantity = len(issues_data)
+    return issues_quantity
 
 
 if __name__ == '__main__':
